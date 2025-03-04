19e39891f0ae6d926f114b912e6e4e5a65eee8d0
Pavel <93135086+paveldotsmirnov@users.noreply.github.com>
2025-03-04 13:36:30 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,11 +3,22 @@ const cors = require('cors')
 const app = express()
 const port = 5000
 
-app.use(cors({
-  origin: '*', // Allow all origins (change this in production)
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+const allowedOrigins = ["http://localhost"]
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      console.log('Origin not allowed by CORS:', origin)
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200,
+  credentials: true
+}
+
+app.use(cors(corsOptions));
 
 app.get('/api', (req, res) => {
   res.json({ message: 'CORS-enabled API!' });
