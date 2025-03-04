6d262ccca928b68ffad650e4ba79aa47d201d73b
Pavel <93135086+paveldotsmirnov@users.noreply.github.com>
2025-03-04 01:26:38 +0100

    add react-router

---
+++ client/src/RecentActivity.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function RecentActivity() {
+  return (
+    <div>RecentActivity
+      <a href="/dashboard/project/1">Project 1</a>
+      <a href="/dashboard/project/2">Project 2</a>
+      <a href="/dashboard/project/3">Project 3</a>
+    </div>
+  )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Routes } from "react-router";
 import reportWebVitals from './reportWebVitals';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import RecentActivity from './RecentActivity';
+import Project from './Project';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="dashboard" element={<Dashboard />}>
+        <Route index element={<RecentActivity />} />
+        <Route path="project/:id" element={<Project />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   </React.StrictMode>
 );
 
